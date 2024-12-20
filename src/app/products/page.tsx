3868c29eacfa95ed3d971cd8cb7e5a474030737d
Cahya Juniar <juniarcahya@gmail.com>
2024-12-21 02:03:47 +0700

    fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,15 @@
 import ProductsPageWrapper from "@/interfaces/components/productsPage/ProductsPageWrapper";
 import api from "@/services/api/api";
 import { cookies } from "next/headers";
-export default async function Page({ params, searchParams }: { params: Record<string, string>; searchParams: Promise<{ search: string }> }) {
+export default async function Page({ params, searchParams }: any) {
   const { search } = await searchParams;
   const cookieList = await cookies();
   const viewport = cookieList.get("viewport")?.value;
   const userId = cookieList.get("userId")?.value;
+  const token = cookieList.get("token")?.value;
 
   // Menggunakan ternary operator untuk memilih API call
   const products = search ? await api.getProductsByName(search) : await api.getAllProducts();
 
-  return <ProductsPageWrapper products={products || []} viewport={viewport} userId={Number(userId)} />;
+  return <ProductsPageWrapper products={products || []} viewport={viewport} userId={Number(userId)} token={token} />;
 }
